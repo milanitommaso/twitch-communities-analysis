07dfe57f06523ab1b23ac71a6ffb6fe0644da409
milani <milani.tommaso2004@gmail.com>
2023-12-20 13:40:13 +0100

    use bot instead of main account

--- data_downloader/config.py
+++ data_downloader/config.py
@@ -1,7 +1,7 @@
 import json
 
-BOT_OWNER = 'milanitommaso'
-NICK = 'milanitommaso'
+BOT_OWNER = 'milanitommasobot'
+NICK = 'milanitommasobot'
 SERVER = 'irc.twitch.tv'
 N_STREAMS = 200
 CHECK_CHANNELS_TIME = 600
