e844791e36d4cb4377f7b43920ec9fa00051e5ab
milanitommaso <milani.tommaso2004@gmail.com>
2024-02-20 15:22:02 +0100

    changed from percentage top chatters to absolute values

--- analysis_scripts/top_contributors_impact.py
+++ analysis_scripts/top_contributors_impact.py
@@ -7,10 +7,24 @@ import datetime
 sys.path.append(os.path.join(os.path.dirname(__file__), '..', "data_downloader"))
 from config import *
 
-TOP_CONTRIBUTORS_PERCENTAGE = 0.01
+TOP_N_CHATTERS = [10, 50, 100]
+
+
+def get_top_streamers(number_of_streamers=50):
+    top_streamers = []
+
+    with open('top_streamers.txt', 'r') as f:
+        lines = f.readlines()[0:number_of_streamers]
+
+    for l in lines:
+        top_streamers.append(l.strip())
+
+    return top_streamers
+
 
 def get_timestamp_from_filename(filename):
     return datetime.datetime.strptime(filename, "chat_%y-%m-%d_%H-%M-%S.txt")
+
 
 def get_chats_dataframes_to_analyze(directory: str):
     chats_dataframes = {}   # key: channel name, value: dataframes
@@ -72,6 +86,7 @@ def get_dataframe_from_chat_file(chat_filename):
     # read the csv file
     try:
         chat_df = pd.read_csv(chat_filename, sep='\t', names=['timestamp', 'is_mod', 'is_sub', 'user', 'message'])
+        chat_df = chat_df[['timestamp', 'user']]
     except:
         print("Error reading file: ", chat_filename)
         return chat_df
@@ -85,20 +100,15 @@ def get_dataframe_from_chat_file(chat_filename):
     return chat_df
 
 
-def get_top_contributors(chat_df):
-    # get the total number of messages
-    total_messages = chat_df.shape[0]
-
+def get_top_contributors(chat_df, top_n_contributors: int):
     # get the number of messages sent by each user
     user_messages = chat_df['user'].value_counts()
 
     # sort the users by number of messages
     user_messages.sort_values(ascending=False, inplace=True)
 
-    # get the number of messages of the last user
-
-    # take the first TOP_CONTRIBUTORS_PERCENTAGE of the users
-    top_contributors_messages = user_messages.head(int(total_messages * TOP_CONTRIBUTORS_PERCENTAGE))
+    # take the messages of the top_n_contributors
+    top_contributors_messages = user_messages.head(top_n_contributors)
 
     # get the top contributors
     top_contributors = top_contributors_messages.index.tolist()
@@ -116,12 +126,64 @@ def get_top_contributors_impact(chat_df, top_contributors):
     top_contributors_messages_count = chat_df[chat_df['user'].isin(top_contributors)].shape[0]
 
     # get the impact of the top contributors
-    impact = top_contributors_messages_count / total_messages
+    impact = (top_contributors_messages_count / total_messages) * 100
 
     return impact
 
 
-if __name__ == "__main__":
+def get_total_impact(saved_impacts):
+    # saved_impacts = {channel_name: {top10: [impact1, impact2, ...], top50:[impact3, impact4]}, ...}
+
+    top_streamers = get_top_streamers()
+
+    for channel_name in saved_impacts:
+        for top_n in saved_impacts[channel_name]:
+            saved_impacts[channel_name][top_n] = sum(saved_impacts[channel_name][top_n]) / len(saved_impacts[channel_name][top_n])
+            # use only 2 decimal places
+            saved_impacts[channel_name][top_n] = round(saved_impacts[channel_name][top_n], 2)
+
+    # get the average impact
+    average_impacts = {}
+    for channel_name in saved_impacts:
+        for top_n in saved_impacts[channel_name]:
+            if top_n not in average_impacts:
+                average_impacts[top_n] = []
+            average_impacts[top_n].append(saved_impacts[channel_name][top_n])
+
+    for top_n in average_impacts:
+        average_impacts[top_n] = sum(average_impacts[top_n]) / len(average_impacts[top_n])
+        average_impacts[top_n] = round(average_impacts[top_n], 2)
+
+    saved_impacts['total'] = average_impacts
+
+    # get the average impact only for top 50 streamers
+    average_impacts_top_streamers = {}
+    for channel_name in saved_impacts:
+        if channel_name not in top_streamers:
+            continue
+
+        for top_n in saved_impacts[channel_name]:
+            if top_n not in average_impacts_top_streamers:
+                average_impacts_top_streamers[top_n] = []
+            average_impacts_top_streamers[top_n].append(saved_impacts[channel_name][top_n])
+
+    for top_n in average_impacts_top_streamers:
+        average_impacts_top_streamers[top_n] = sum(average_impacts_top_streamers[top_n]) / len(average_impacts_top_streamers[top_n])
+        average_impacts_top_streamers[top_n] = round(average_impacts_top_streamers[top_n], 2)
+
+    saved_impacts['total_top_streamers'] = average_impacts_top_streamers
+
+    # sort the channels by name
+    saved_impacts = {k: v for k, v in sorted(saved_impacts.items(), key=lambda item: item[0])}
+
+    # move the totals impact to the start
+    saved_impacts = {'total': saved_impacts['total'], **saved_impacts}
+    saved_impacts = {'total_top_streamers': saved_impacts['total_top_streamers'], **saved_impacts}
+
+    return saved_impacts
+
+
+def main():
     saved_impacts = {}
     chats_dataframes = {}   # key: channel name, value: dataframes
 
@@ -133,46 +195,34 @@ if __name__ == "__main__":
     bar.start()
 
     for i, (channel_name, dataframes) in enumerate(chats_dataframes.items()):
-        # get the channel name from the filename
-
         for df in dataframes:
-            top_contributors = get_top_contributors(df)
+            for top_n in TOP_N_CHATTERS:
+                top_n_str = "top" + str(top_n)
 
-            impact = get_top_contributors_impact(df, top_contributors)
+                top_n_contributors = get_top_contributors(df, top_n)
 
-            if channel_name not in saved_impacts:
-                saved_impacts[channel_name] = []
-            saved_impacts[channel_name].append(impact)
-        
+                if len(top_n_contributors) != top_n:
+                    continue
+
+                impact = get_top_contributors_impact(df, top_n_contributors)
+
+                if channel_name not in saved_impacts:
+                    saved_impacts[channel_name] = {}
+                if top_n_str not in saved_impacts[channel_name]:
+                    saved_impacts[channel_name][top_n_str] = []
+
+                saved_impacts[channel_name][top_n_str].append(impact)
+                
         bar.update(i + 1)
 
     bar.finish()
-
-    # get the total impact
-    sum_impact = 0
-    count = 0
-
-    for channel_name in saved_impacts:
-        if sum(saved_impacts[channel_name]) != 0:
-            sum_impact += sum(saved_impacts[channel_name])
-            count += len(saved_impacts[channel_name])
-
-    saved_impacts['total'] = '%.2f'%((sum_impact / count) * 100)
-
-    # get the average impact for every channel
-    for channel_name in saved_impacts:
-        if channel_name == 'total':
-            continue
-        saved_impacts[channel_name] = '%.2f'%((sum(saved_impacts[channel_name]) / len(saved_impacts[channel_name])) * 100)
-
-    # sort the channels by name
-    saved_impacts = {k: v for k, v in sorted(saved_impacts.items(), key=lambda item: item[0])}
-
-    # move the total impact to the start
-    saved_impacts = {'total': saved_impacts['total'], **saved_impacts}
-
-    print(saved_impacts)
+    
+    saved_impacts = get_total_impact(saved_impacts)
 
     # save the impacts in json
-    with open('top_contributors_impact.json', 'w') as f:
+    with open('analysis_results/top_contributors_impact.json', 'w') as f:
         json.dump(saved_impacts, f, indent=4)
+
+
+if __name__ == "__main__":
+    main()
